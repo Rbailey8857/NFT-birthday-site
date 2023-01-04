7debacfd0da0c7879918e8a246e49c356e11edcb
Rbailey8857 <80913719+Rbailey8857@users.noreply.github.com>
2023-01-04 06:52:43 -0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,7 @@ module.exports = {
         marquee: 'marquee 5s linear infinite',
         marquee2: 'marquee2 5s linear infinite',
       },
-      animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
-      },
       keyframes: {
-        spin: {
-          '0deg': { transform: 'rotate(0deg)' },
-          '360deg': { transform: 'rotate(360deg)' }
-        },
         marquee: {
           '0%': { transform: 'translateX(0%)' },
           '100%': { transform: 'translateX(-100%)' },
